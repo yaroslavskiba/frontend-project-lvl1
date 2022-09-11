fed2d4dc40192177cea4073bed5fca2cf4f1898d
yaroslavs skiba <yaroslavskiba322@gmail.com>
2022-09-11 10:36:17 +0300

    edit

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 import helloName from '../src/cli.js';
-import evenNumber from './brain-even.js';
 
 console.log('Welcome to the Brain Games!');
-helloName();
-evenNumber();
+console.log(helloName());
